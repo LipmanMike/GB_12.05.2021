41d9af2f0d3bb37b6f91886c63dc2e7a15d29d91
LipmanMike <lypka.michael@gmail.com>
2021-05-16 18:16:56 +0300

    Начал делать ДЗ второго урока

---
+++ Lesson 2/test.py
@@ -0,0 +1,5 @@
+winter_months = ['december', 'december', 'december', 'january', 'february']
+winter_months.append(12)
+winter_months.append([1, 2])
+print(winter_months.count('december'))
+print(winter_months.index('january'))
